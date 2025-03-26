f1d54be25934f0c27756d63068c32a518203e7fe
root <root@DESKTOP-CNB5O32.>
2025-03-27 00:18:43 +0100

    done task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,3 @@
-export default function getBudgetObject(income=400, gdp=700, capita=900) {
-    const budget = {
-      income: income,
-      gdp: gdp,
-      capita: capita,
-    };
-  
-    return budget;
-  }
+export default function getBudgetObject(income = 400, gdp = 700, capita = 900) {
+  return { income, gdp, capita };
+}
