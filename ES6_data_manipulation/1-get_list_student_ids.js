5f3f8be3b0672bd3acb837ed0e18c2b5df41b891
root <root@DESKTOP-CNB5O32.>
2025-04-02 05:58:18 +0200

    fixing codestyle for the task 0,1,2,3,4

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(listStudents) {
-    if (!Array.isArray(listStudents)) {
-      return [];
-    }
-    return listStudents.map((student) => student.id);
+  if (!Array.isArray(listStudents)) {
+    return [];
   }
+  return listStudents.map((student) => student.id);
+}
--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
-export default function updateStudentGradeByCity(students, city, newGrades)   {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter((student) => student.location === city);
   return studentsInCity.map((student) => {
     const studentGrades = newGrades.find((grade) => grade.studentId === student.id);
