50087c954457150c3fd6a6d46bed844540886c8a
root <root@DESKTOP-CNB5O32.>
2025-04-02 06:54:23 +0200

    task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,12 @@
 export default function getResponseFromAPI(success) {
-    return new Promise((resolve) => {
-      resolve({
-        statusCode: 200,
-        body: 'Success',
-      });
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          statusCode: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The API call failed'));
+      }
     });
   }
