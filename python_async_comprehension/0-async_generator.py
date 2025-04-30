39db9d992fdf26938200878bb36b8504a207d7ef
root <root@DESKTOP-CNB5O32.>
2025-04-30 10:42:20 +0200

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,6 @@ import random
 
 async def async_generator():
     """Asynchronously yield 10 random numbers between 0 and 10, one every second."""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
