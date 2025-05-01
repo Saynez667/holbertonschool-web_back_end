26e3ce56621b660319185f2134fa2ea5ea9c97e7
root <root@DESKTOP-CNB5O32.>
2025-05-01 10:09:13 +0200

    pycodestyle for task 0 and add comments

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Helper module for pagination index range calculation
+"""
+
+
 def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = page * page_size
